04cb4887ffecc4f3f2f36a49511adb341d46657c
EMCain <>
2018-03-25 19:42:41 -0700

    test that Player exists in sample scene

---
+++ Assets/Tests/Editor/SampleSceneTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class SampleSceneTest {
+
+	[Test]
+	public void VerifySceneContents() {
+		EditorSceneManager.OpenScene(
+			"Assets/Scenes/sample.unity", 
+			OpenSceneMode.Single
+		);
+		GameObject player = GameObject.Find("Player"); 
+		Assert.IsNotNull(player, "Player object was not found in scene!");
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator NewEditModeTest1WithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
